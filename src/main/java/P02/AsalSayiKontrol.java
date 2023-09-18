74276b7984e6cd563892d212d904b41d62fbe692
fikretzeybek <fzeybek1972@gmail.com>
2023-09-18 08:17:13 +0300

    ilave

---
+++ src/main/java/P02/AsalSayiKontrol.java
@@ -0,0 +1,46 @@
+package P02;
+
+import java.util.Scanner;
+
+public class AsalSayiKontrol {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayi giriniz: ");
+        int sayi = scanner.nextInt();
+
+        if (sayi == 0) {
+            System.out.println("Girdiğiniz sayı 0, program sonlandırıldı.");
+        } else {
+            boolean asalMi = isAsal(sayi);
+
+            String sonuc = asalMi ? "Asal Sayidir" : "Asal Degildir";
+            System.out.println(sayi + " Sayisi " + sonuc);
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isAsal(int sayi) {
+        if (sayi <= 1) {
+            return false;
+        }
+
+        if (sayi <= 3) {
+            return true;
+        }
+
+        if (sayi % 2 == 0 || sayi % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= sayi; i += 6) {
+            if (sayi % i == 0 || sayi % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
