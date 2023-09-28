e390e588baf010dc4f79cd12aa60ff0d21912335
fikretzeybek <fzeybek1972@gmail.com>
2023-09-28 07:38:12 +0300

    "p07"

---
+++ src/main/java/P07/DenklemForLoop.java
@@ -0,0 +1,17 @@
+package P07;
+
+public class DenklemForLoop {
+
+    public static void main(String[] args) {
+        // ∑ 3*x2 + 2*x + 5 denkleminin 0-100 arasındaki
+        // sonucunu bulun:
+
+
+        int sonuc = 0;
+
+        for (int x = 0; x < 100; x++) {
+            sonuc += 3 * (x * x) + 2 * x + 5;
+        }
+        System.out.println("Sonuc = " + sonuc);
+    }
+}
